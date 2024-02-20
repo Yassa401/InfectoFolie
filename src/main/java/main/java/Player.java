bf0913e0737d108015914a7e39943a34f90aad5b
Yassa401 <assalyasser001@gmail.com>
2024-02-20 16:43:28 +0100

    Traitement des collisions murs en cours
    la méthode de gestion des collisions avec les murs est plus fluide, un seul soucis : la rencontre avec deux obstacles au même temps ne donne pas le bon résultat

--- src/main/java/main/java/Player.java
+++ src/main/java/main/java/Player.java
@@ -73,10 +73,46 @@ public class Player {
     public boolean verifCollisionMur(int newX, int newY) {
         for (Rectangle2D mur : GameFrame.murs) {
                 // Collision détectée
-        		if (mur.intersects(newX, newY, getRadius(), getRadius())) {
-        			System.out.println("mur \n\n\n\n\n\n\n\n\n\n\n");
-        			return true;
-        		}
+        		/*if (mur.intersects(newX, getY(), getRadius(), getRadius())) {
+        			if(mur.intersects(getX(), newY, getRadius(), getRadius())) {
+        				return true ;
+        			}else {
+        				y = newY;
+        				return true;
+        			}
+        		}else {
+	        		if(mur.intersects(getX(), newY, getRadius(),getRadius())) {
+	        			if(mur.intersects(newX, getY(), getRadius(), getRadius())) {
+	        				return true ;
+	        			}else {
+		        			x = newX ;
+	        				System.out.println("mur \n\n\n");
+	        				return true;
+	        			}
+        			}else {
+        				if(mur.intersects(newX, newY, getRadius(),getRadius())) {
+        					return true ;
+        				}
+        			}
+        		}*/
+        	// Teste avec les deux nouvelles coordonnees d'abord
+	        if(mur.intersects(newX, newY, getRadius(),getRadius())) {
+	        	// Teste chacun des coordonnées comme ça l'autre peut avoir une nouvelle valeur
+	        	// si ça ne rentre pas dans l'obstacle (ça rend le déplacement plus fluide)
+	        	if (mur.intersects(newX, getY(), getRadius(), getRadius())) {
+	    			if(mur.intersects(getX(), newY, getRadius(), getRadius())) {
+	    				return true ;
+	    			}else {
+	    				y = newY;
+	    				return true;
+	    			}
+	    		}else {
+        			x = newX ;
+    				System.out.println("mur \n\n\n");
+    				return true;	
+	    		}
+	        }
+	        
         }
         return false;
     }
