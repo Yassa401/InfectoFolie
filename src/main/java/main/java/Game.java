32cf2a29874bfec3bf2fb8db03d417300b80b1fe
alpha <alphadiallo.mb@gmail.com>
2024-03-01 23:29:12 +0100

    Définition des méthodes pour obtenir le nb de joueurs vivants/morts, le nb à infecté, le nb non infecté actu

--- src/main/java/main/java/Game.java
+++ src/main/java/main/java/Game.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import java.util.Iterator;
 import java.util.Map;
 
 public class Game {
@@ -18,18 +19,69 @@ public class Game {
 	}
 	
 	public int getNbLivingPlayers() {
-		// boucle pour calculer le nb de joueurs vivants
-		return 0;
+		int res = 0;
+		for(Map.Entry<String, Player> entry : this.players.entrySet()) {
+			if(entry.getValue().getStatut() != 2) {
+				res++;
+			}
+		}
+		
+		return res;
 	}
 	
 	public int getNbDeadPlayers() {
-		// boucle pour calculer le ng joueurs morts
-		return 0;
+		int res = 0;
+		for(Map.Entry<String, Player> entry : this.players.entrySet()) {
+			if(entry.getValue().getStatut() == 2) {
+				res++;
+			}
+		}
+		return res;
+	}
+	
+	public int getNbPlayers2Infect() {
+		int nbPlayers = this.players.size();
+		int nb2infec = 0;
+		
+		if(nbPlayers <= 5) {
+			nb2infec = 1;
+		}
+		else if(nbPlayers <= 20) {
+			nb2infec = 3;
+		}
+		else if(nbPlayers <= 40) {
+			nb2infec = 5;
+		}
+		else if(nbPlayers <= 70) {
+			nb2infec = 7;
+		}
+		else if(nbPlayers <= 100) {
+			nb2infec = 10;
+		}
+
+		return nb2infec;
 	}
 	
 	// méthode qui met à jour le nb des jours au cours du jeu
-	private void updatePlayers() {
-		//...
+	private void updateNbPlayers() {
+		// utiliser iterator, car on ne peut supp un element dans liste qu'on parcours
+		Iterator<Map.Entry<String, Player>> iterator = this.players.entrySet().iterator();
+		while(iterator.hasNext()) {
+			Map.Entry<String, Player> entry = iterator.next();
+			if(entry.getValue().getStatut() == 2) {
+				iterator.remove();
+			}
+		}
+	}
+	
+	// infecte un joueurs (changer son statut et sa couleur)
+	public void infectPlayer(Player p) {
+		
+	}
+	
+	// utilise les joueurs pour une partit du jeu
+	public void infectPlayers() {
+		
 	}
 	
 	// méthode qui effectue le jeu
